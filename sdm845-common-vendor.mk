08f26b579b063f9fbe139f4c9db533251cc8cec0
PainKiller3 <ninadpatil100@gmail.com>
2021-10-19 00:09:16 +0530

    sdm845-common: Update MIUI offmode charging daemon
    * From qssi-user-12-SKQ1.210908.001-21.10.12-release-keys
    * Fixes offline charging
    
    Change-Id: I768f1b4eb9c03a32125b96c69d7719c8f72bdb6e
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -6,6 +6,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/sdm845-common
 
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/sdm845-common/proprietary/system/bin/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/chargeonlymode \
     vendor/xiaomi/sdm845-common/proprietary/product/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptXHD_encoder.so \
     vendor/xiaomi/sdm845-common/proprietary/product/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptX_encoder.so \
     vendor/xiaomi/sdm845-common/proprietary/system/bin/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/chargeonlymode \
